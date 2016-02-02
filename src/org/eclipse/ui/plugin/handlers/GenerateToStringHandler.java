1014f0fa7b9d9dc2b86cb57c081b9425a57b51c8
ALTAN ŞENEL <altan.senel@gunessigorta.com.tr>
2016-02-02 17:10:25 +0200

    right click popup items

--- src/org/eclipse/ui/plugin/handlers/GenerateToStringHandler.java
+++ src/org/eclipse/ui/plugin/handlers/GenerateToStringHandler.java
@@ -26,16 +26,21 @@ import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
 import org.eclipse.jdt.core.dom.rewrite.ListRewrite;
 import org.eclipse.jdt.ui.JavaUI;
+import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.text.Document;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.text.edits.MalformedTreeException;
 import org.eclipse.text.edits.TextEdit;
 import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.plugin.Activator;
 import org.eclipse.ui.plugin.utils.EclipseConstants;
 
-public class GenerateToStringHandler extends AbstractHandler {
+public class GenerateToStringHandler extends AbstractHandler implements IObjectActionDelegate {
 	
 
 	@Override
@@ -51,67 +56,65 @@ public class GenerateToStringHandler extends AbstractHandler {
 		if (EclipseConstants.JAVA_EDITOR_ID.equals(activePartId)) {
 			// get edited file
 			IEditorInput input = HandlerUtil.getActiveEditorInput(event);
+			generateToStringMethod(input);
 
+//			IJavaElement elem = JavaUI.getEditorInputJavaElement(input);
+//			if (elem.exists()) {
+//
+//				ICompilationUnit unit = (ICompilationUnit) elem;
+//				// parse compilation unit
+//				
+//				CompilationUnit astRoot = createAST(unit);
+//
+//				List<IField> fieldlist = new ArrayList<IField>();
+//				try {
+//					for (IField field : unit.getTypes()[0].getFields()) {
+//						fieldlist.add(field);
+//					}
+//				} catch (JavaModelException e1) {
+//					// TODO Auto-generated catch block
+//					e1.printStackTrace();
+//				}
+//					
+//
+//				// create a ASTRewrite
+//				AST ast = astRoot.getAST();
+//				ASTRewrite rewriter = ASTRewrite.create(ast);
+//
+//				astRoot.recordModifications();
+//
+//
+//				Block block =ast.newBlock();	
+//				block = createToStringMethodStatements(ast,fieldlist);
+//				
+//				MethodDeclaration methodDeclaration2 = createToStringMethod(ast, Modifier.PUBLIC, false, block);
+//
+//
+//				ListRewrite lrw2 = rewriter.getListRewrite(((TypeDeclaration) astRoot.types().get(0)), TypeDeclaration.BODY_DECLARATIONS_PROPERTY);
+//				lrw2.insertLast(methodDeclaration2, null);
+//				
+//
+//				try {
+//					TextEdit edits = rewriter.rewriteAST();
+//					Document document = new Document(unit.getSource());
+//					edits.apply(document);
+//					// this is the code for adding statements
+//					unit.getBuffer().setContents(document.get());
+//				} catch (JavaModelException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				} catch (IllegalArgumentException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				} catch (MalformedTreeException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				} catch (org.eclipse.jface.text.BadLocationException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//			}
 
-			IJavaElement elem = JavaUI.getEditorInputJavaElement(input);
-			if (elem.exists()) {
-
-				ICompilationUnit unit = (ICompilationUnit) elem;
-				// parse compilation unit
-				
-				CompilationUnit astRoot = createAST(unit);
-
-				List<IField> fieldlist = new ArrayList<IField>();
-				try {
-					for (IField field : unit.getTypes()[0].getFields()) {
-						fieldlist.add(field);
-					}
-				} catch (JavaModelException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-					
-
-				// create a ASTRewrite
-				AST ast = astRoot.getAST();
-				ASTRewrite rewriter = ASTRewrite.create(ast);
-
-				astRoot.recordModifications();
-
-
-				Block block =ast.newBlock();	
-				block = createToStringMethodStatements(ast,fieldlist);
-				
-				MethodDeclaration methodDeclaration2 = createToStringMethod(ast, Modifier.PUBLIC, false, block);
-
-
-				ListRewrite lrw2 = rewriter.getListRewrite(((TypeDeclaration) astRoot.types().get(0)), TypeDeclaration.BODY_DECLARATIONS_PROPERTY);
-				lrw2.insertLast(methodDeclaration2, null);
-				
-
-				try {
-					TextEdit edits = rewriter.rewriteAST();
-					Document document = new Document(unit.getSource());
-					edits.apply(document);
-					// this is the code for adding statements
-					unit.getBuffer().setContents(document.get());
-				} catch (JavaModelException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IllegalArgumentException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (MalformedTreeException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (org.eclipse.jface.text.BadLocationException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-
-			String a = "asdasdsad";
-			a.getBytes();
 
 			// currentSelection now contains text selection inside input file
 			// ... locate class selected in that file ...
@@ -218,4 +221,90 @@ public class GenerateToStringHandler extends AbstractHandler {
 	}
 
 
+	@Override
+	public void run(IAction action) {
+		IEditorPart editorPart = Activator.getDefault().getWorkbench()
+				.getActiveWorkbenchWindow().getActivePage()
+				.getActiveEditor();
+		IEditorInput editor = editorPart.getEditorInput();
+		generateToStringMethod(editor);
+		
+	}
+
+
+	@Override
+	public void selectionChanged(IAction arg0, ISelection arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void setActivePart(IAction arg0, IWorkbenchPart arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void generateToStringMethod(IEditorInput editor) {
+		
+		IJavaElement elem = JavaUI.getEditorInputJavaElement(editor);
+		if (elem.exists()) {
+
+			ICompilationUnit unit = (ICompilationUnit) elem;
+			// parse compilation unit
+			
+			CompilationUnit astRoot = createAST(unit);
+
+			List<IField> fieldlist = new ArrayList<IField>();
+			try {
+				for (IField field : unit.getTypes()[0].getFields()) {
+					fieldlist.add(field);
+				}
+			} catch (JavaModelException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+				
+
+			// create a ASTRewrite
+			AST ast = astRoot.getAST();
+			ASTRewrite rewriter = ASTRewrite.create(ast);
+
+			astRoot.recordModifications();
+
+
+			Block block =ast.newBlock();	
+			block = createToStringMethodStatements(ast,fieldlist);
+			
+			MethodDeclaration methodDeclaration2 = createToStringMethod(ast, Modifier.PUBLIC, false, block);
+
+
+			ListRewrite lrw2 = rewriter.getListRewrite(((TypeDeclaration) astRoot.types().get(0)), TypeDeclaration.BODY_DECLARATIONS_PROPERTY);
+			lrw2.insertLast(methodDeclaration2, null);
+			
+
+			try {
+				TextEdit edits = rewriter.rewriteAST();
+				Document document = new Document(unit.getSource());
+				edits.apply(document);
+				// this is the code for adding statements
+				unit.getBuffer().setContents(document.get());
+			} catch (JavaModelException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (MalformedTreeException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (org.eclipse.jface.text.BadLocationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}	
+	}
+
 }
+
+
